4725cf6f0762f54137e8052b0224ebcd0c737785
Ryan Tran <tranryan005@gmail.com>
2024-11-12 00:15:23 -0500

    Webpage dual workies

---
+++ webPage.py
@@ -0,0 +1,18 @@
+# Web Design 
+
+import io
+import logging
+import socketserver
+from http import server
+from threading import Condition
+import webbrowser
+
+hostName = "localhost"
+serverPort = 9420
+
+#lePage = codecs.open('pageHtml.html', 'r', 'utc-8')
+webbrowser.open('pageHtml.html')
+
+
+
+
